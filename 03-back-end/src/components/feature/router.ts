4d253f8b3fca2a0318a34090a77da2a03dedacd2
Milan Đorđević - 2017200804 <djordjevicmilan0304@gmail.com>
2021-09-16 15:18:47 +0200

    TO bi trebalo da bude to :D

--- 03-back-end/src/components/feature/router.ts
+++ 03-back-end/src/components/feature/router.ts
@@ -2,15 +2,16 @@ import * as express from "express";
 import IApplicationResources from '../../common/IApplicationResources.interface';
 import IRouter from '../../common/IRouter.interface';
 import FeatureController from './controller';
+import AuthMiddleware from '../../middleware/auth.middleware';
 
 export default class FeatureRouter implements IRouter {
     public setupRoutes(application: express.Application, resources: IApplicationResources) {
         const featureController: FeatureController = new FeatureController(resources);
 
-        application.get("/feature/:id",           featureController.getById.bind(featureController));
-        application.get("/category/:cid/feature", featureController.getAllInCategory.bind(featureController));
-        application.post("/feature",              featureController.add.bind(featureController));
-        application.put("/feature/:id",           featureController.edit.bind(featureController));
-        application.delete("/feature/:id",        featureController.delete.bind(featureController));
+        application.get("/feature/:id",     AuthMiddleware.getVerifier("administrator", "user"),       featureController.getById.bind(featureController));
+        application.get("/category/:cid/feature", AuthMiddleware.getVerifier("administrator", "user"), featureController.getAllInCategory.bind(featureController));
+        application.post("/feature",       AuthMiddleware.getVerifier("administrator"),        featureController.add.bind(featureController));
+        application.put("/feature/:id",     AuthMiddleware.getVerifier("administrator"),      featureController.edit.bind(featureController));
+        application.delete("/feature/:id",  AuthMiddleware.getVerifier("administrator"),      featureController.delete.bind(featureController));
     }
 }
--- 03-back-end/src/components/user/router.ts
+++ 03-back-end/src/components/user/router.ts
@@ -2,16 +2,17 @@ import IRouter from '../../common/IRouter.interface';
 import { Application } from 'express';
 import IApplicationResources from '../../common/IApplicationResources.interface';
 import UserController from './controller';
+import AuthMiddleware from '../../middleware/auth.middleware';
 
 export default class UserRouter implements IRouter {
     public setupRoutes(application: Application, resources: IApplicationResources) {
         const userController = new UserController(resources);
 
-        application.get("/user",         userController.getAll.bind(userController));
-        application.get("/user/:id",     userController.getById.bind(userController));
-        application.post("/user",        userController.add.bind(userController));
-        application.put("/user/:id",     userController.edit.bind(userController));
-        application.delete("/user/:id",  userController.delete.bind(userController));
-        application.post("/auth/user/register", userController.register.bind(userController));
+        application.get("/user",   AuthMiddleware.getVerifier("administrator","user"),      userController.getAll.bind(userController));
+        application.get("/user/:id",   AuthMiddleware.getVerifier("administrator","user"),  userController.getById.bind(userController));
+        application.post("/user",     AuthMiddleware.getVerifier("administrator"),   userController.add.bind(userController));
+        application.put("/user/:id",  AuthMiddleware.getVerifier("administrator"),   userController.edit.bind(userController));
+        application.delete("/user/:id",AuthMiddleware.getVerifier("administrator"),  userController.delete.bind(userController));
+        application.post("/auth/user/register",AuthMiddleware.getVerifier("administrator"), userController.register.bind(userController));
     }
 }
